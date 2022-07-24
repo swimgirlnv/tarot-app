1bf117835d57524652f4dcb65df8a91f0941b588
Chris Waterson <waterson@gmail.com>
2022-07-24 09:26:00 -0700

    Use localStorage intead of sessionStorage.

--- src/tarot-frontend/tarot/src/useToken.tsx
+++ src/tarot-frontend/tarot/src/useToken.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react';
 
 export default function useToken() {
   const getToken = () => {
-    return sessionStorage.getItem('token') || '';
+    return localStorage.getItem('token') || '';
   };
 
   const [token, setToken] = useState(getToken());
 
   const saveToken = (userToken: any) => {
-    sessionStorage.setItem('token', userToken);
+    localStorage.setItem('token', userToken);
     setToken(userToken);
   };
 
