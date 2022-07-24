aefd272e6ad4cdf587ed65f400f6ecbecffc4d37
Chris Waterson <waterson@gmail.com>
2022-07-24 10:11:12 -0700

    Style the login page a bit.

--- src/tarot-frontend/tarot/src/Login.tsx
+++ src/tarot-frontend/tarot/src/Login.tsx
@@ -6,17 +6,33 @@ import './Login.css';
 export default function Login(setTokenThingy: any) {
   return(
     <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <GoogleOAuthProvider clientId="87928211828-tgq8r75ntrl2d3qn6nujnldl9i99rsct.apps.googleusercontent.com">
-      <GoogleLogin
-        onSuccess={(credentialsResponse) => {
-          setTokenThingy.setToken(credentialsResponse.credential);
-        }}
-        onError={() => {
-          console.log("Login failed");
-        }}
-      />
-      </GoogleOAuthProvider>
+      <h1>
+        Welcome to Major Arcana!
+      </h1>
+
+      <div>
+        This site is currently only open for use by a select group
+        of initiates, who may log in using Google.
+      </div>
+
+      <div className="oauth-wrapper">
+        <GoogleOAuthProvider clientId="87928211828-tgq8r75ntrl2d3qn6nujnldl9i99rsct.apps.googleusercontent.com">
+        <GoogleLogin
+          onSuccess={(credentialsResponse) => {
+            setTokenThingy.setToken(credentialsResponse.credential);
+          }}
+         onError={() => {
+            console.log("Login failed");
+          }}
+        />
+        </GoogleOAuthProvider>
+      </div>
+
+      <div>
+        If you would like to become an initiate or are having trouble,
+        please <a href="mailto:oracle@majorarcana.net">contact The Oracle</a>!
+      </div>
+
       </div>
   )
 }
